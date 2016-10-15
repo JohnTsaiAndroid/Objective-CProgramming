42cf2c5a12f500bdfb5945efc637a1cbe3bd25b2
JohnTsai <johntsai.work@gmail.com>
2016-10-15 23:54:42 +0800

    update

--- 1015/firstOCClass.m
+++ 1015/firstOCClass.m
@@ -29,10 +29,17 @@
     int speed;
     
 }
+//OC对象的方法，需以减号(-)开头
+//OC方法中的小括号用于括住数据类型
+-(void)run;
 @end
 
 @implementation Car
 
+-(void)run
+{
+    NSLog(@"The car is running!");
+}
 
 @end
 
@@ -40,9 +47,17 @@ int main()
 {
     //执行Car类的new行为创建对象
     Car * car = [Car new];
+    Car * anotherCar = [Car new];
+    
     car->wheels = 4;
     car->speed = 120;
+    
+    //给anotherCar所指向的对象发送run消息
+    [anotherCar run];
+    
     NSLog(@"%@",car);
+    NSLog(@"%@",anotherCar);
+    NSLog(@"%lu",sizeof(int));
     NSLog(@"wheels = %d,speed = %d",car->wheels,car->speed);
     return 0;
 }
